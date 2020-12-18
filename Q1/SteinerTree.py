4ab7fc2031a527c5da8e022c8fba102385a90b5a
Arash Hajisafi <34424672+Arashhs@users.noreply.github.com>
2020-12-19 01:47:57 +0330

    Changed crossover and selection functions, added more configurability options, added check to see if all terminals are connected, PERFORMANCE GREATLY IMPROVED

--- Q1/SteinerTree.py
+++ Q1/SteinerTree.py
@@ -1,6 +1,6 @@
 import math
 import matplotlib.pyplot as plt
-from ypstruct import struct
+from ypstruct import structure
 import numpy as np
 import ga
 
@@ -13,6 +13,9 @@ terminal_vert = [] # Terminal Vertices
 vertices = []
 graph = [] # Our graph
 edges = []
+
+ss = [] # subset with all indices of terminal vertices
+
 
 #add a vertex to the graph
 def add_vertex(v):
@@ -29,6 +32,7 @@ def add_vertex(v):
         temp.append(0)
     graph.append(temp)
 
+
 # Add an edge between vertices v1 and v2 with a weight of e
 def add_edge(v1_index, v2_index):
     global graph
@@ -41,13 +45,22 @@ def add_edge(v1_index, v2_index):
     graph[v2_index][v1_index] = edge_cost
 
 
+def build_graph(edges_arr):
+    global vertices_num
+    res_graph = [ [0] * vertices_num for _ in range(vertices_num)]
+    for e in edges_arr:
+        if e.deleted == False:
+            res_graph[e.v1][e.v2] = res_graph[e.v2][e.v1] = e.cost
+    return res_graph
+
+
 def init(file_name):
-    global steiner_vert_num, terminal_vert_num, edges_num, graph
+    global steiner_vert_num, terminal_vert_num, edges_num, graph, ss
     with open(file_name, 'r') as reader:
         steiner_vert_num, terminal_vert_num, edges_num = [int(x) for x in next(reader).split()] # read first line
         for i in range(steiner_vert_num):
             x, y = [int(x) for x in next(reader).split()] # read steiner vertices coordinates
-            v = struct()
+            v = structure()
             v.x = x
             v.y = y
             steiner_vert.append(v)
@@ -55,18 +68,19 @@ def init(file_name):
 
         for i in range(terminal_vert_num):
             x, y = [int(x) for x in next(reader).split()] # read terminal vertices coordinates
-            v = struct()
+            v = structure()
             v.x = x
             v.y = y
             terminal_vert.append(v)
             add_vertex(v)
+            ss.append(i+steiner_vert_num)
 
         graph = [[0 for x in range(vertices_num)] for y in range(vertices_num)] # Creating empty 2d vertex list
 
         for i in range(edges_num):
             v1_index, v2_index = [int(x) for x in next(reader).split()] # read vertices corresponding to each edge
             add_edge(v1_index, v2_index)
-            edge = struct()
+            edge = structure()
             edge.v1 = v1_index
             edge.v2 = v2_index
             edge.deleted = False
@@ -82,9 +96,23 @@ def calculate_cost(graph_in):
 
 def calculate_cost2(edges_arr):
     cost = sum(e.cost for e in edges_arr if e.deleted == False) / 2
+    cor_graph = build_graph(edges_arr)
+    # is_a_tree = is_tree(cor_graph)
+    terminals_connected = are_terminals_connected(cor_graph) 
+    return cost, terminals_connected[0], terminals_connected[1]
+
+def calculate_cost3(edges_arr):
+    cost, terminals_connected, cc = calculate_cost2(edges_arr)
+    if terminals_connected == False:
+         cost = cost + 10000 * len(cc)
     return cost
 
-def plot_graph():
+def calculate_cost4(edges_arr):
+    cost, terminals_connected, cc = calculate_cost2(edges_arr)
+    return cost
+
+
+def plot_graph(graph_in):
     terminal_vertices_x = [vertex.x for vertex in terminal_vert]
     terminal_vertices_y = [vertex.y for vertex in terminal_vert]
     plt.scatter(terminal_vertices_x, terminal_vertices_y, label= "Data", color= "red", linewidths=5, s=30)
@@ -93,14 +121,38 @@ def plot_graph():
     steiner_vertices_y = [vertex.y for vertex in steiner_vert]
     plt.scatter(steiner_vertices_x, steiner_vertices_y, label= "Data", color= "blue", s=30)
 
-    for i in range(len(graph)):
-        for j in range(len(graph)):
-            if graph[i][j] != 0:
+    for i in range(len(graph_in)):
+        for j in range(len(graph_in)):
+            if graph_in[i][j] != 0:
                 x_values = [vertices[i].x, vertices[j].x]
                 y_values = [vertices[i].y, vertices[j].y]
                 #print("({}, {}) -> ({}, {})".format(vertices[i][0], vertices[i][1], vertices[j][0], vertices[j][1]))
                 plt.plot(x_values, y_values, color='blue', linewidth=1)
     plt.show()
+
+
+def plot_graph_vertices():
+    terminal_vertices_x = [vertex.x for vertex in terminal_vert]
+    terminal_vertices_y = [vertex.y for vertex in terminal_vert]
+    plt.scatter(terminal_vertices_x, terminal_vertices_y, label= "Data", color= "red", linewidths=5, s=30)
+
+    steiner_vertices_x = [vertex.x for vertex in steiner_vert]
+    steiner_vertices_y = [vertex.y for vertex in steiner_vert]
+    plt.scatter(steiner_vertices_x, steiner_vertices_y, label= "Data", color= "blue", s=30)
+
+    plt.show()
+    return plt.plot([], [], color='blue', linewidth=1)
+
+
+'''
+def update_line(hl, graph_in):
+    for i in range(len(graph_in)):
+        for j in range(len(graph_in)):
+            if graph_in[i][j] != 0:
+                x_values = [vertices[i].x, vertices[j].x]
+                y_values = [vertices[i].y, vertices[j].y]
+                #print("({}, {}) -> ({}, {})".format(vertices[i][0], vertices[i][1], vertices[j][0], vertices[j][1]))
+                plt.plot(x_values, y_values, color='blue', linewidth=1) '''
 
 
 # Checks whether there is a cycle in graph or not
@@ -144,6 +196,47 @@ def is_tree(graph_in):
     return True
 
 
+def DFSUtil(temp, v, visited, graph_in):
+    # current vertex is now visited
+    visited[v] = True
+
+    # Store the vertex to list
+    temp.append(v)
+
+    # Repeat for all adjacent vertices to this vertex
+    for i in range (len(vertices)):
+        if graph_in[v][i] != 0:
+
+            if visited[i] == False:
+
+                # Update the list
+                temp = DFSUtil(temp, i, visited, graph_in)
+    return temp
+ 
+
+# Method to find connected components in an undirected graph
+def connectedComponents(graph_in):
+    global ss
+    visited = []
+    cc = []
+    for i in range(vertices_num):
+        visited.append(False)
+    for v in range(vertices_num):
+        if visited[v] == False and v >= steiner_vert_num:
+            temp = []
+            cc.append(DFSUtil(temp, v, visited, graph_in))
+    return cc
+
+def are_terminals_connected(graph_in):
+    global ss
+    cc = connectedComponents(graph_in)
+    for i in range(len(cc)):
+        if all(elem in cc[i] for elem in ss):
+            return True, cc
+    return False, cc
+    
+
+
 def print_debugging():
     print(steiner_vert_num, terminal_vert_num, edges_num)
     print("Vertices:", vertices)
@@ -157,36 +250,53 @@ def print_debugging():
 
 def main():
     
-    #init("steiner_in.txt")
+    init("steiner_in.txt")
     #init("test.txt")
-    init("test2.txt")
-    plot_graph()
+    #init("test2.txt")
+    #init("test3.txt")
+    #plot_graph(graph)
+
+    #print(are_terminals_connected(graph))
+    #print(connectedComponents(graph))
+    #print(ss)
 
     ncost = calculate_cost(graph)
     wcost = calculate_cost2(edges)
 
+    plot_graph(graph)
+
     print("Tree which contants all of the terminal nodes?", ncost, wcost)
 
     # Defining the problem
-    problem = struct()
+    problem = structure()
     problem.graph = graph
-    problem.cost = calculate_cost2
+    problem.cost = calculate_cost3
     problem.nvar = int(len(vertices)/2) # number of variables
     problem.steiner_vert = steiner_vert
     problem.terminal_vert = terminal_vert
     problem.vertices = vertices
     problem.edges = edges
+    problem.plot_graph = plot_graph
+    problem.build_graph = build_graph
+    problem.calculate_cost4 = calculate_cost4
 
 
     # Defining the parameters
-    params = struct()
-    params.npop = 5 # ancestors population number
-    params.pc = 5 # children population number
+    params = structure()
+    params.npop = 6 # ancestors population number
+    params.pc = 6 # children population number
     params.maxit = 100 # maximum number of iterations
     params.npoints = 2 # number of points for crossover
+    params.mu = 0.2 # mutation probability
+    params.beta = 1
 
     # running the GA algorithm
     ga.run(problem, params)
+
+    # plot graph
+
+
+
 
 
 
--- Q1/ga.py
+++ Q1/ga.py
@@ -1,4 +1,4 @@
-from ypstruct import struct
+from ypstruct import structure
 import numpy as np
 import bisect # for fast sorting lists
 import copy
@@ -7,13 +7,16 @@ def copy_edges_arr(edges_arr):
     edges_tmp = [edge.deepcopy() for edge in edges_arr]
     return edges_tmp
 
-def calculate_edges_bit_arr(edges_arr):
+def bit_arr_to_edges(edges_bit_arr, edges_arr):
+    edges_res = copy_edges_arr(edges_arr)
+    for i in range(len(edges_arr)):
+        edges_res[i].deleted = True if edges_bit_arr[i] == 0 else False
+    return edges_res
+
+def edges_to_bit_arr(edges_arr):
     bit_arr = [None] * len(edges_arr)
     for i in range(len(bit_arr)):
         bit_arr[i] = 0 if edges_arr[i].deleted else 1
-    print("BitArr:", bit_arr)
-    for i in range(len(bit_arr)):
-        print("i:", edges_arr[i].deleted)
     return bit_arr
         
 
@@ -23,8 +26,8 @@ def delete_edge(chromosome, index):
 def single_point_crossover(A, B, x):
     A_temp = np.array(A)
     B_temp = np.array(B)
-    A_new = np.append(A_temp[:x], B_temp[x:]).toList()
-    B_new = np.append(B_temp[:x], A_temp[x:]).toList()
+    A_new = np.append(A_temp[:x], B_temp[x:]).tolist()
+    B_new = np.append(B_temp[:x], A_temp[x:]).tolist()
     return A_new, B_new
 
 # X denotes an array of crossover points
@@ -41,6 +44,22 @@ def uniform_crossover(A, B, P):
             B[i] = temp
     return A, B
 
+# mu is the probability of mutation for each bit
+def mutation(A, mu):
+    res = [i for i in A]
+    flag = np.random.rand(len(A)) <= mu
+    for i in range(len(flag)):
+        if flag[i]:
+            res[i] = 1 if A[i] == 0 else 0
+    return res
+
+def roulette_wheel_selection(p):
+    c = np.cumsum(p)
+    r = sum(p) * np.random.rand()
+    ind = np.argwhere(r <= c)
+    return ind[0][0]
+
+
 
 
 def run(problem, params):
@@ -53,6 +72,9 @@ def run(problem, params):
     terminal_vert = problem.terminal_vert
     vertices = problem.vertices
     edges = problem.edges 
+    plot_graph = problem.plot_graph
+    build_graph = problem.build_graph
+    calculate_cost4 = problem.calculate_cost4
 
 
     # parameters
@@ -61,12 +83,14 @@ def run(problem, params):
     pc = params.pc
     # nc is the number of children, which is pc times the number of parents
     # first divided by 2 and rounded, then multiplied by 2 to ensure that nc is an even number
-    nc = np.round(pc*nc/2)*2 
+    nc = int(np.round(pc*npop/2)*2) 
     npoints = params.npoints
+    mu = params.mu
+    beta = params.beta
 
 
     # empty chromosome
-    empty_chromosome = struct()
+    empty_chromosome = structure()
     empty_chromosome.cost = None
     empty_chromosome.edges = None
     empty_chromosome.edges_bit_arr = None
@@ -92,24 +116,24 @@ def run(problem, params):
         for rand in random_positions:
             delete_edge(pop[i], int(round(rand)))
         pop[i].cost = cost(pop[i].edges)
-        pop[i].edges_bit_arr = calculate_edges_bit_arr(pop[i].edges)
+        pop[i].edges_bit_arr = edges_to_bit_arr(pop[i].edges)
 
         if pop[i].cost < best_chromosome.cost:
             best_chromosome.edges = copy_edges_arr(pop[i].edges)
             best_chromosome.cost = pop[i].cost
             best_chromosome.edges_bit_arr = [i for i in pop[i].edges_bit_arr]
-            print(best_chromosome.edges_bit_arr)
 
         if pop[i].cost > worst_chromosome.cost:
-           worst_chromosome.cost = pop[i].cost
-           worst_chromosome.edges_bit_arr = [i for i in pop[i].edges_bit_arr]
+            worst_chromosome.edges = copy_edges_arr(pop[i].edges)
+            worst_chromosome.cost = pop[i].cost
+            worst_chromosome.edges_bit_arr = [i for i in pop[i].edges_bit_arr]
 
     print("Initial Population:")
     avg_chromosome.cost = sum(p.cost for p in pop)/npop
     print("{identity:<7} Chromosome: fitness = {fitness:.8f}, cost = {cost:.3f}".format(identity="Best", fitness=1/best_chromosome.cost, cost=best_chromosome.cost))
     print("{identity:<7} Chromosome: fitness = {fitness:.8f}, cost = {cost:.3f}".format(identity="Worst", fitness=1/worst_chromosome.cost, cost=worst_chromosome.cost))
     print("{identity:<7} Chromosome: fitness = {fitness:.8f}, cost = {cost:.3f}".format(identity="Average", fitness=1/avg_chromosome.cost, cost=avg_chromosome.cost))
-    calculate_edges_bit_arr(best_chromosome.edges)
+    print("\n")
 
     # best cost of iterations
     bestcost = np.empty(maxit)
@@ -118,6 +142,12 @@ def run(problem, params):
 
     # Main loop
     for it in range(maxit):
+
+        costs = np.array([p.cost for p in pop])
+        avg_cost = np.mean(costs)
+        if avg_cost != 0:
+            costs = costs/avg_cost # normalization
+        probs = np.exp(-beta*costs)
         
         popc = [] # population of children
         
@@ -125,17 +155,89 @@ def run(problem, params):
         for k in range(nc//2): # half of nc times (integer division)
             
             # select parents
-            q = np.random.permutation(npop) # a random permutation from integers 0 to npop - 1
-            p1 = pop[q[0]]
-            p2 = pop[q[1]]
+            # q = np.random.permutation(npop) # a random permutation from integers 0 to npop - 1
+            # p1 = pop[q[0]]
+            # p2 = pop[q[1]]
+
+            # perform RW (Roulette Wheel) selection
+            p1 = pop[roulette_wheel_selection(probs)]
+            p2 = pop[roulette_wheel_selection(probs)]
 
             # perform crossover
             c1 = empty_chromosome.deepcopy()
             c2 = empty_chromosome.deepcopy()
             p = np.random.rand(len(p1.edges_bit_arr))
             c1.edges_bit_arr, c2.edges_bit_arr = uniform_crossover(p1.edges_bit_arr, p2.edges_bit_arr, p)
+            # p = np.random.randint(len(edges), size=npoints)
+            # c1.edges_bit_arr, c2.edges_bit_arr = multi_point_crossover(p1.edges_bit_arr, p2.edges_bit_arr, p)
 
+
+            c1.edges = bit_arr_to_edges(c1.edges_bit_arr, edges)
+            c2.edges = bit_arr_to_edges(c2.edges_bit_arr, edges)
+
+            
             # perform mutation
+            c1.edges_bit_arr = mutation(c1.edges_bit_arr, mu)
+            c2.edges_bit_arr = mutation(c2.edges_bit_arr, mu)
+
+            # update c1 and c2 edges
+            c1.edges = bit_arr_to_edges(c1.edges_bit_arr, c1.edges)
+            c1.edges = bit_arr_to_edges(c2.edges_bit_arr, c2.edges)
+
+            # evaluate first offspring
+            c1.cost = cost(c1.edges)
+            if c1.cost < best_chromosome.cost:
+                best_chromosome.edges = copy_edges_arr(c1.edges)
+                best_chromosome.cost = c1.cost
+                best_chromosome.edges_bit_arr = [i for i in c1.edges_bit_arr]
+
+            if c1.cost > worst_chromosome.cost:
+                worst_chromosome.edges = copy_edges_arr(c1.edges)
+                worst_chromosome.cost = c1.cost
+                worst_chromosome.edges_bit_arr = [i for i in c1.edges_bit_arr]
+
+            # evaluate second offspring
+            c2.cost = cost(c2.edges)
+            if c2.cost < best_chromosome.cost:
+                best_chromosome.edges = copy_edges_arr(c2.edges)
+                best_chromosome.cost = c2.cost
+                best_chromosome.edges_bit_arr = [i for i in c2.edges_bit_arr]
+
+            if c1.cost > worst_chromosome.cost:
+                worst_chromosome.edges = copy_edges_arr(c2.edges)
+                worst_chromosome.cost = c2.cost
+                worst_chromosome.edges_bit_arr = [i for i in c2.edges_bit_arr]
+
+            # add offsprings to popc
+            popc.append(c1)
+            popc.append(c2)
+
+        # merge, sort, and select
+        pop += popc # Lambda + mu selection
+        pop = sorted(pop, key=lambda p: p.cost)
+        pop = pop[0:npop] # top npop population
+
+        # store best cost
+        bestcost[it] = best_chromosome.cost
+
+        # print information
+        avg_chromosome.cost = sum(p.cost for p in pop)/npop
+        print("Iteration:", it)
+        print("{identity:<7} Chromosome: fitness = {fitness:.8f}, cost = {cost:.3f}".format(identity="Best", fitness=1/best_chromosome.cost, cost=best_chromosome.cost))
+        print("{identity:<7} Chromosome: fitness = {fitness:.8f}, cost = {cost:.3f}".format(identity="Worst", fitness=1/worst_chromosome.cost, cost=worst_chromosome.cost))
+        print("{identity:<7} Chromosome: fitness = {fitness:.8f}, cost = {cost:.3f}".format(identity="Average", fitness=1/avg_chromosome.cost, cost=avg_chromosome.cost))
+        print("\n")
+    
+    best_chromosome.cost = calculate_cost4(best_chromosome.edges)
+    plot_graph(build_graph(best_chromosome.edges))
+    print("Final Best Cost:", best_chromosome.cost)
+        
+
+    # output
+    out = structure()
+    out.pop = pop
+
+
 
 
 
